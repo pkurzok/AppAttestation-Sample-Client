8f423cb11dc5892d7eae24631ef942dca3ef8656
Peter Kurzok <peter.kurzok@maibornwolff.de>
2025-01-16 16:32:08 +0100

    Initial Commit

---
+++ AppAttestationClient/AppAttestationClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppAttestationClientApp.swift
+//  AppAttestationClient
+//
+//  Created by Peter Kurzok on 16.01.25.
+//
+
+import SwiftUI
+
+@main
+struct AppAttestationClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
